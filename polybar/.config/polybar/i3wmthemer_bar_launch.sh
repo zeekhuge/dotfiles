4899687cf7b82885a90eb7e2ed0e2d66b0926f75
ZeekHuge <me.zubeen3@gmail.com>
2020-08-21 23:17:07 +0530

    polybar: Add bottom bar

--- polybar/.config/polybar/i3wmthemer_bar_launch.sh
+++ polybar/.config/polybar/i3wmthemer_bar_launch.sh
@@ -6,7 +6,8 @@ sleep 1;
 
 if type "xrandr"; then
     for m in $(xrandr --query | grep "\s\+connected" | cut -d " " -f1); do
-        MONITOR=$m polybar i3wmthemer_bar &
+        MONITOR=$m polybar i3wmthemer_bar_top &
+        MONITOR=$m polybar i3wmthemer_bar_bottom &
     done
 else
     polybar i3wmthemer_bar &
